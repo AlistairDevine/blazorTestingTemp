7bacbf8905619867289e7479fa037344b6e52b1e
alistairdevine <alistair.devine98@gmail.com>
2020-06-25 23:36:45 +0100

    Form and validation
    Counter page has an added edit form and validation model. Ready for the controller to be added with data flow.

---
+++ testingTemp/testingTemp/Models/RepairForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace testingTemp.Models
+{
+    public class RepairForm
+    {
+        [Required]
+        [StringLength(20, MinimumLength =5, ErrorMessage ="Not valid name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="No notes taken")]
+        public string Notes { get; set;}
+        [Required(ErrorMessage ="Trade not selected")]
+        public string Trade { get; set; }
+        [Required]
+        [Range(0.05, 12, ErrorMessage ="Not valid amount of time")]
+        public string Hours { get; set; }
+
+    }
+}
